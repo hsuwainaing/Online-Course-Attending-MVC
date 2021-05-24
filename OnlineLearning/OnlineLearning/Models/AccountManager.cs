a4e384689988f86176e1a18d5a17a3ccca482475
Hsu Wai Naing <hsuwainaing@koekoetech.com>
2021-05-24 15:36:01 +0630

    Register

---
+++ OnlineLearning/OnlineLearning/Models/AccountManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLearning.Models
+{
+    public class AccountManager
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get;set;}
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        public string Email { get; set; }
+
+        [Required]
+
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public DateTime ProcessingDate { get; set; }
+
+    }
+}
---
+++ OnlineLearning/OnlineLearning/Models/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace OnlineLearning.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DBConnection")
+
+        {
+
+        }
+        public DbSet<AccountManager> AccountManagerSet { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<AccountManager>().ToTable("AccountManager");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
