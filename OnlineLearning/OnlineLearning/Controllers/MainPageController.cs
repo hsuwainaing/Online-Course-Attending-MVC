c1a46ba4fa5667576aa4e58b0e0164ecc012095f
Hnin Phyuphyu <Hnin Phyuphyu@ademcosecurity.local>
2021-05-25 11:11:02 +0800

    Register&Login

---
+++ OnlineLearning/OnlineLearning/Controllers/MainPageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Onlinecourseattendance.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult Index()
+        {
+            //string username = Request.Cookies["UserName"].Value;
+            string username = string.Empty;
+            //OR
+            HttpCookie reqCookies = Request.Cookies["UserInfo"];  
+            if(reqCookies!=null)
+            {
+                username = reqCookies["UserName"].ToString();
+            }
+            ViewBag.Name = username;
+            return View();
+        }
+    }
+}
